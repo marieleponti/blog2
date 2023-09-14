8c3032edb8a831a0b8b4d3d27583e8bc0e2fa9cf
marieleponti <marieleponti@gmail.com>
2023-09-14 16:27:07 -0500

    start basic templates, posts dictionary

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'mariele',
+        'title': 'blog post 1',
+        'content': 'my content',
+        'date': '14 Sept 2023'
+    },
+
+    {
+        'author': 'Lola',
+        'title': 'blog post 2',
+        'content': 'her content',
+        'date': '13 Sept 2023'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts': posts}
+    return render(request, 'blog_app/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog_app/about.html')
