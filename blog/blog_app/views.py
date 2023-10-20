9eb750b6262cfb22d268c8e00866c2ff613f026e
marieleponti <marieleponti@gmail.com>
2023-10-20 13:47:03 -0500

    add ListView, DetailView, CreateView to blog app, add url paths, add form for new post creation

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView, CreateView
+
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog_app/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog_app/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
 
 
 def about(request):
