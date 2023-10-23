bf82f6e0ef23ed30cb8286f535ac6ad7fb75dfff
marieleponti <marieleponti@gmail.com>
2023-10-23 14:48:26 -0500

    create, update, delete posts w user verif, LoginReq & UserPassesTest mixins

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,7 +1,9 @@
+from django.forms.models import BaseModelForm
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 def home(request):
@@ -20,9 +22,41 @@ class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if(post.author == self.request.user):
+            return True
+        else: 
+            return False
+        
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url='/'
+
+    def test_func(self):
+        post = self.get_object()
+        if(post.author == self.request.user):
+            return True
+        else: 
+            return False
 
 
 def about(request):
