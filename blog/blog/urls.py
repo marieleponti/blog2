5714b7bb2fad459be6026ec956162e2820463d8f
marieleponti <marieleponti@gmail.com>
2023-10-27 11:21:01 -0500

    accept *args, **kwargs in save method in user model

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html', success_url=reverse_lazy('password-reset-done')), name='password-reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password-reset-confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password-reset-complete'),
     path('', include('blog_app.urls'))
 ]
 
