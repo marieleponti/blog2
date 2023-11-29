367aa35c96da159a982f4f6a47989260cbfe0fb6
mariele <marieleponti@gmail.com>
2023-11-29 11:18:21 -0600

    update commit

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cbr4jmu*$*9xniru2x#hommtel7&xcje9n)zz+!y&q)7je--8j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','sharedshelves.org']
+ALLOWED_HOSTS = ['127.0.0.1','sharedshelves.org']
 
 
 # Application definition
